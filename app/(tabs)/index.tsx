595a19a44bf10c84bca387e657411ef492f72f59
OfficeB <office@ih-club.com>
2024-12-05 15:01:50 +0100

    Add useTranslations hook and update TabOneScreen component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,16 @@
 import { StyleSheet } from 'react-native';
 
 import { Text, View } from '@/components/Themed';
-import { useTranslation } from "react-i18next";
 import ChangeLanguage from '@/components/ChangeLanguage';
+import { useTranslations } from '@/hooks/useTranslations';
 
 export default function TabOneScreen() {
-  const { t } = useTranslation();
-  const text = t('helloWorld');
+  const dictionary = useTranslations();
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
-        {text}
+        {dictionary('helloWorld')}
       </Text>
       <ChangeLanguage />
     </View>
